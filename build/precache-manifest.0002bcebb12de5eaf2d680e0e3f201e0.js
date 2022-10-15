c9a30c9dd5832828bb1d03a100534a6f7780f07b
Pablo071181 <pablo071181@gmail.com>
2022-10-15 06:01:33 -0500

    Run Build

---
+++ build/precache-manifest.0002bcebb12de5eaf2d680e0e3f201e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "60f5a57cca3f2431c9c0",
+    "url": "/Restaurant_Frontend/static/css/main.4a40a9fd.chunk.css"
+  },
+  {
+    "revision": "60f5a57cca3f2431c9c0",
+    "url": "/Restaurant_Frontend/static/js/main.60f5a57c.chunk.js"
+  },
+  {
+    "revision": "8c6c46e92f59f65aaa3a",
+    "url": "/Restaurant_Frontend/static/js/1.8c6c46e9.chunk.js"
+  },
+  {
+    "revision": "fe23d49ab9275065a01e",
+    "url": "/Restaurant_Frontend/static/css/2.4d5f528f.chunk.css"
+  },
+  {
+    "revision": "fe23d49ab9275065a01e",
+    "url": "/Restaurant_Frontend/static/js/2.fe23d49a.chunk.js"
+  },
+  {
+    "revision": "00e37794a2f93a40ceb4",
+    "url": "/Restaurant_Frontend/static/js/runtime~main.00e37794.js"
+  },
+  {
+    "revision": "7fd2d4cb17df68181c8aac55d7806f5d",
+    "url": "/Restaurant_Frontend/index.html"
+  }
+];
--- src/components/Offline.js
+++ src/components/Offline.js
@@ -11,7 +11,7 @@ const Offline = () => {
         <Form.Label>Déjanos tu correo electrónico</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+          Por favor ingrese sus datos
         </Form.Text>
       </Form.Group>
 
